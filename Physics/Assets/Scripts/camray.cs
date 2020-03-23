fd04d916d43997f2550ed221b3d6f86ccbeca213
Liu Guoqing <woshihaodayikeshu@163.com>
2020-03-23 21:50:09 +0800

    Add Physics Project.

---
+++ Physics/Assets/Scripts/camray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camray : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //从摄像机到单击处发出射线
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                //画出射线, 只有在Scene视图中才能看到
+                Debug.DrawLine(ray.origin, hitInfo.point);
+                GameObject gameObj = hitInfo.collider.gameObject;
+                Debug.Log("click object name is" + gameObj.name);
+                //当射线碰撞目标的标签是Pickup时, 执行拾取操作
+                if (gameObj.tag == "Pickup")
+                {
+                    Debug.Log("pick up!");
+                }
+            }
+        }
+    }
+}
---
+++ Physics/Assets/Scripts/mousedrag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousedrag : MonoBehaviour
+{
+    void OnMouseDrag()
+    {
+        if (this.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Input.GetAxis("Mouse X") * 5, Input.GetAxis("Mouse Y") * 5, 0);
+        }
+        else
+        {
+            transform.position += Vector3.right * Time.deltaTime * Input.GetAxis("Mouse X");
+            transform.position += Vector3.up * Time.deltaTime * Input.GetAxis("Mouse Y");
+        }
+    }
+}
